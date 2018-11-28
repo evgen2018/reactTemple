de8762e2621b504e8254e1a2619489859f41c132
evgen2018 <timofeev111@ukr.net>
2018-11-28 19:59:32 +0200

    Updates

---
+++ precache-manifest.fdd6eb2241690bf9eec5c2bd8adc8fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactTemple/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "34bc239b59a68d3dc946",
+    "url": "/reactTemple/static/js/runtime~main.34bc239b.js"
+  },
+  {
+    "revision": "a9592331f9ab542e965f",
+    "url": "/reactTemple/static/js/main.a9592331.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/reactTemple/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "a9592331f9ab542e965f",
+    "url": "/reactTemple/static/css/main.05072ce6.chunk.css"
+  },
+  {
+    "revision": "2c944cbe4cd7f1c1c9e16ce31c48f527",
+    "url": "/reactTemple/index.html"
+  }
+];
