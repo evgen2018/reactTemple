6017eba2e449d434136a293a7646ce58118e3da5
evgen2018 <timofeev111@ukr.net>
2018-11-29 14:30:32 +0200

    comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from  './Header';
 import ProgressBlock from  './ProgressBlock';
 import Vocabulary from  './Vocabulary';
 import Footer from  './Footer';
@@ -9,7 +8,7 @@ import Footer from  './Footer';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
           <header className="header">
               <ProgressBlock/>
               <Vocabulary/>
